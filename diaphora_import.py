daacadbae7cda54d1cfa3bb68b8bd0fc552646f6
Joxean <joxean@coseinc.com>
2015-07-15 17:34:19 +0200

    Added script to import only structs & enums + minor modifications
    FEATURE: Added script diaphora_import.py to import only structures, unions and enumerations from .sqlite exported databases.
    HEUR: Remove the initial underscores in the function name prior to save.

---
+++ diaphora_import.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+
+"""
+Diaphora, a diffing plugin for IDA
+Copyright (c) 2015, Joxean Koret
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+
+import diaphora
+reload(diaphora)
+from diaphora import import_definitions
+
+from PySide import QtGui
+
+#-----------------------------------------------------------------------
+def main():
+  import_definitions()
+
+if __name__ == "__main__":
+  main()
