a90824788b19df4e21ddd5520d47dc1c5b623adf
Niklas Baumstark <niklas.baumstark@gmail.com>
2019-06-26 00:00:00 +0200

    Don't overwrite true_name with demangled name
    Otherwise demangled names will be used verbatim yielding weird results

--- diaphora_ida.py
+++ diaphora_ida.py
@@ -1392,7 +1392,6 @@ or selecting Edit -> Plugins -> Diaphora - Show results""")
 
     if demangled_name is not None:
       name = demangled_name
-      true_name = name
 
     if self.hooks is not None:
       ret = self.hooks.before_export_function(f, name)
